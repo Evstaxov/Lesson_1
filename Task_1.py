41645378c4bb52720a1314e77ce2e391e0f5b35f
EvgeniySM <normalllive@mail.ru>
2020-06-15 13:45:08 +0300

    Lesson_1

---
+++ Task_1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name - ")
+print(f"Hi {name}:)")
+age = input("How old are you? - ")
+print(f"{age} ,no, you look much younger.")
+yes = "yes"
+status = input("are you married?(Yes or No) - ")
+if status == yes:
+    print(f"Then I wish you happiness, goodbye, {name})")
+else:
+    print(f"Still to come, {name}, goodbye)")
